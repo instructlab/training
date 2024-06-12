da65a3272f54fc571e5aaab0c9b3ca52ad392737
Russell Bryant <rbryant@redhat.com>
2024-06-12 09:48:23 -0400

    Create a polished interface for the instructlab training library.
    Signed-off-by: Oleg S <97077423+RobotSail@users.noreply.github.com>

---
+++ src/instructlab/training/config.py
@@ -0,0 +1,132 @@
+"""
+Collection of config objects used in the InstructLab training library.
+"""
+
+from enum import Enum
+from pydantic import BaseModel, ConfigDict, Field
+
+
+class DeepSpeedOffloadStrategy(Enum):
+    """
+    Defines the offload strategy for DeepSpeed.
+
+    To learn more, read about it here: https://www.deepspeed.ai/tutorials/zero-offload/
+    """
+
+    CPU = "cpu"
+    # TODO: update this when we support ZeRO stage-3
+    # https://github.com/instructlab/training/issues/26
+    # NVME = "nvme"
+    NONE = None
+
+
+class QuantizeDataType(Enum):
+    """
+    Defines what datatype we use during quantization.
+    """
+
+    NF4 = "nf4"
+    # FP8 = "fp8" TODO: test and evaluate fp8
+    NONE = None
+
+
+class DataProcessArgs(BaseModel):
+    """
+    All the arguments consumed by the training data pre-process script.
+    """
+
+    data_path: str
+    data_output_path: str
+    max_seq_len: int  # defines the max sequence length of a sample
+    model_path: str  # either a HF model name or path to HF model
+
+    # disable the protected namespace for the model_config field
+    model_config = ConfigDict(protected_namespaces=())
+
+
+class TorchrunArgs(BaseModel):
+    """
+    Representation of the arguments being used by torchrun.
+    The full list of arguments can be found here:
+    https://pytorch.org/docs/stable/elastic/run.html#definitions
+    """
+
+    nproc_per_node: int
+    nnodes: int
+    node_rank: int
+    rdzv_id: int
+    rdzv_endpoint: str
+
+
+class LoraOptions(BaseModel):
+    """
+    Options to specify when training using a LoRA.
+    """
+
+    rank: int = 4
+    alpha: float = 32
+    dropout: float = 0.1
+    target_modules: list[str] = Field(
+        default_factory=lambda: ["q_proj", "k_proj", "v_proj", "o_proj"]
+    )
+    quantize_data_type: QuantizeDataType = QuantizeDataType.NONE
+
+
+class DeepSpeedOptions(BaseModel):
+    """
+    Represents the available options we support when training with the DeepSpeed optimizer.
+    For more information, please read:
+    https://www.deepspeed.ai/docs/config-json/
+
+    Defaults are all taken from the above docs.
+    """
+
+    cpu_offload_optimizer: bool = False
+    cpu_offload_optimizer_ratio: float = 1
+    cpu_offload_optimizer_pin_memory: bool = False
+
+
+class TrainingArgs(BaseModel):
+    """
+    This class represents the arguments being used by the training script.
+    """
+
+    # disable the protected namespace for the model_config field
+    model_config = ConfigDict(protected_namespaces=())
+
+    # Either the name of a HuggingFace model or a path to a model saved in HuggingFace format.
+    model_path: str
+
+    # this field specifies the filepath to the training dataset before processing
+    data_path: str
+    ckpt_output_dir: str
+
+    # this field defines where we should be saving the processed version of the training dataset
+    # after we have tokenized it
+    data_output_dir: str
+
+    max_seq_len: int
+    max_batch_len: int
+    num_epochs: int
+    effective_batch_size: int
+    save_samples: int
+    learning_rate: float
+    warmup_steps: int
+    is_padding_free: bool
+    random_seed: int = 42
+
+    mock_data: bool = False
+    mock_data_len: int = 0
+
+    deepspeed_options: DeepSpeedOptions = Field(
+        default_factory=lambda: DeepSpeedOptions(
+            cpu_offload_optimizer=False,
+            cpu_offload_optimizer_ratio=1,
+            cpu_offload_optimizer_pin_memory=False,
+        )
+    )
+
+    # TODO(osilkin): support quantized full fine-tuning:
+    # https://github.com/instructlab/training/issues/28
+    # quantize_dtype: QuantizeDataType = QuantizeDataType.NONE
+    lora: LoraOptions | None = None
--- src/instructlab/training/tokenizer_utils.py
+++ src/instructlab/training/tokenizer_utils.py
@@ -1,7 +1,7 @@
 from dataclasses import dataclass, field
 from transformers import AutoTokenizer
 
-from ilab_train.utils import log_rank_0
+from instructlab.training.utils import log_rank_0
 
 
 @dataclass
