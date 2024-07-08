677bc750d3280c6420cfb4440ca8eed8c77fa44f
aldo pareja-cardona <aldo.pareja@ibm.com>
2024-07-08 17:39:12 -0400

    rebase resolution

--- src/instructlab/training/main_ds.py
+++ src/instructlab/training/main_ds.py
@@ -241,7 +241,7 @@ def setup_model(args, tokenizer, train_loader, grad_accum):
         )
 
     lr_scheduler = get_scheduler(
-        name="constant_with_warmup",
+        name=args.lr_scheduler,
         optimizer=optimizer,
         num_warmup_steps=args.num_warmup_steps,
         num_training_steps=args.num_epochs * len(train_loader),
