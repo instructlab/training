32c37425e9a1b72aa2b9f873992c673984024423
Mustafa Eyceoz <meyceoz@redhat.com>
2024-07-17 11:49:40 -0400

    Connect new merging function to main save call
    Signed-off-by: Mustafa Eyceoz <meyceoz@redhat.com>

--- src/instructlab/training/main_ds.py
+++ src/instructlab/training/main_ds.py
@@ -445,6 +445,8 @@ def train(args, model, tokenizer, train_loader, grad_accum, metric_logger):
                     model,
                     tokenizer,
                     global_step * args.samples_per_gpu * world_size,
+                    bool(args.lora_r),
+                    bool(args.lora_r) and bool(args.lora_quant_bits),
                 )
 
             if (
