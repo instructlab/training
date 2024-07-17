2d3bfa6b3b073f44e83e26097d43ac6d0776a0ca
Mustafa Eyceoz <meyceoz@redhat.com>
2024-07-17 00:51:18 -0400

    Updating token masking to support pretraining w/ masked special tokens (#137)
    Data Processing add support for pretraining with padded special tokens, increase flexibility, and include data previews
    
    Signed-off-by: Mustafa Eyceoz <meyceoz@redhat.com>
    
    ---------
    
    Signed-off-by: Mustafa Eyceoz <meyceoz@redhat.com>

--- src/instructlab/training/data_process.py
+++ src/instructlab/training/data_process.py
@@ -1,11 +1,12 @@
 # Standard
+from functools import partial
 from pathlib import Path
-from typing import List
-import logging
 import os
+import random
 
 # Third Party
 from datasets import load_dataset
+from tqdm import tqdm
 from transformers import PreTrainedTokenizer, PreTrainedTokenizerFast
 import numpy as np
 
@@ -27,7 +28,7 @@ def check_valid_sample(
     if len(whole_sentence_tk) >= max_len or len(whole_sentence_tk) < 20:
         return False
     # last token should be eos_token
-    if whole_sentence_tk[-1] != eos_tk:
+    if not eos_tk in (whole_sentence_tk[-1], whole_sentence_tk[-2]):
         return False
 
     special_tokens = [system_tk, assistant_tk, user_tk]
@@ -39,23 +40,7 @@ def check_valid_sample(
     assistant_token_index = (whole_sentence_tk == assistant_tk).nonzero()[0]
     eos_token_index = (whole_sentence_tk == eos_tk).nonzero()[0]
 
-    # check that there are at least one user_token, assistant_token and eos_token
-    if (
-        len(user_token_index) == 0
-        or len(assistant_token_index) == 0
-        or len(eos_token_index) == 0
-    ):
-        print(f"\033[91mthere are no user_token, assistant_token or eos_token\033[0m")
-        log_rank_0(tokenizer.decode(whole_sentence_tk), to_print=True)
-        return False
-
     # check that user_index_token is less than all other indices
-    if len(user_token_index) != len(assistant_token_index):
-        print(
-            "\033[91mthe number of user_token and assistant_token is not the same\033[0m"
-        )
-        log_rank_0(tokenizer.decode(whole_sentence_tk), to_print=True)
-        return False
     if (
         user_token_index[0] > assistant_token_index[0]
         or user_token_index[0] > eos_token_index[0]
@@ -64,132 +49,138 @@ def check_valid_sample(
         log_rank_0(tokenizer.decode(whole_sentence_tk), to_print=True)
         return False
 
-    # check alternating pattern of user_token and assistant_token
-    for user_token_i, assistant_token_i in zip(user_token_index, assistant_token_index):
-        if user_token_i > assistant_token_i:
-            print("\033[91mthe user_token is after the assistant_token\033[0m")
-            log_rank_0(tokenizer.decode(whole_sentence_tk), to_print=True)
-            return False
-
     return True
 
 
-def unmask_only_assistant_responses(
-    chosen_token, user_token, assist_token, system_tk="<|system|>"
+def unmask_message_content(
+    example, user_token, assist_token, system_token, pretrain_token, pretrain_end_token
 ):
     """
-    Generate a labels tensor for language model training, where the model should predict
-    the assistant's responses within a conversation. The labels for the assistant's responses
-    are unmasked, while the rest of the tokens, including the user's responses and the initial
-    prompt, are masked with a value of -100.
+    Create labels for tokens in a sequence with special handling for pretraining tokens.
+
+    This function processes a sequence of tokens and generates a corresponding labels list.
+    Tokens are masked with -100 unless they are part of the assistant's response or within
+    a pretraining segment. Pretraining segments are marked by `pretrain_token` and
+    `pretrain_end_token`, within which only user, assistant, and system special tokens are masked.
+    It also removes the temporary pretraining tokens from the output 'input_ids'.
 
     Parameters:
-    - chosen_token (dict): A dictionary containing 'input_ids', a 1D tensor of tokenized text.
+    - example (dict): A dictionary containing 'input_ids', a list of token IDs.
     - user_token (int): The token ID representing the user's turn in the conversation.
     - assist_token (int): The token ID representing the assistant's turn in the conversation.
-    - prompt_length (int): The length of the initial prompt in tokens, which should be masked.
+    - system_token (int): The token ID representing the system's turn in the conversation.
+    - pretrain_token (int): The token ID marking the start of a pretraining segment.
+    - pretrain_end_token (int): The token ID marking the end of a pretraining segment.
 
     Returns:
-    - torch.Tensor: A tensor of the same shape as `chosen_token['input_ids']` where each token
-      corresponding to the assistant's response is unmasked (retains its original token ID),
-      and all other tokens are masked with -100.
-
-    The function assumes that each assistant's response is followed by a user's response. It masks
-    the initial prompt and any tokens not part of the assistant's responses. The resulting labels
-    tensor can be used in a language model that is trained to generate text by predicting the next
-    token in a sequence.
+    - dict: A dictionary with two keys:
+        - 'labels': a list of labels for the input tokens, where non-assistant and non-pretraining
+          tokens are masked with -100, and all others retain their original token IDs.
+        - 'input_ids': a list of the original token IDs with pretraining tokens removed.
     """
+    sentence_tk = example["input_ids"]
 
-    assert chosen_token["input_ids"].dim() == 1
-    sentence_legth = chosen_token["attention_mask"].sum().item()
-    labels = chosen_token["input_ids"].clone()
-    whole_sentence = chosen_token["input_ids"][:sentence_legth].clone()
+    def unmask(token, special_tokens):
+        if token in special_tokens:
+            return -100
+        return token
 
-    # pre-training mode
-    if not (
-        system_tk in whole_sentence
-        or user_token in whole_sentence
-        or assist_token in whole_sentence
-    ):
-        return labels
+    def mask(token, *args):
+        return -100
 
-    labels[:sentence_legth] = -100
-    assist_ids = (whole_sentence == assist_token).nonzero(as_tuple=True)[0]
-    user_ids = (whole_sentence == user_token).nonzero(as_tuple=True)[0]
+    def update_mask_function(token, in_pretraining, mask_function):
+        if token == pretrain_token:
+            in_pretraining = True
+            mask_function = unmask
+        elif token == pretrain_end_token:
+            in_pretraining = False
+            mask_function = mask
+        if not in_pretraining:
+            if token == assist_token:  # unmasking because of assistant
+                mask_function = unmask
+            elif token in [
+                user_token,
+                system_token,
+            ]:  # masking because of user or system
+                mask_function = mask
+        return in_pretraining, mask_function
 
-    # Find the first user_id that is greater than each assist_id
-    valid_assist_mask = (assist_ids[:, None] < user_ids).float()
-    first_user_ids_after_assist_ids = user_ids[valid_assist_mask.argmax(dim=1)]
+    labels = [-100] * len(sentence_tk)
+    in_pretraining = False
+    mask_function = None
 
-    # Filter out assist_ids that do not have a corresponding user_id
-    valid_assist_ids = assist_ids[first_user_ids_after_assist_ids != 0]
-    valid_user_ids = first_user_ids_after_assist_ids[
-        first_user_ids_after_assist_ids != 0
+    for i, token in enumerate(sentence_tk):
+        in_pretraining, mask_function = update_mask_function(
+            token, in_pretraining, mask_function
+        )
+        labels[i] = mask_function(token, [user_token, assist_token, system_token])
+
+    # Find indices of pretrain tokens and remove them from sentence and labels
+    pretrain_indices = [
+        i
+        for i, token in enumerate(sentence_tk)
+        if token in [pretrain_token, pretrain_end_token]
+    ]
+    final_sentence_tk = [
+        token for i, token in enumerate(sentence_tk) if i not in pretrain_indices
+    ]
+    final_labels = [
+        label for i, label in enumerate(labels) if i not in pretrain_indices
     ]
 
-    # Assign labels for each valid assist_id-user_id pair (without including the assist_id nor the user_id)
-    for assist_id, user_id in zip(valid_assist_ids, valid_user_ids):
-        labels[assist_id + 1 : user_id] = whole_sentence[assist_id + 1 : user_id]
+    for label, token in zip(final_labels, final_sentence_tk):
+        assert label == -100 or token not in [
+            user_token,
+            assist_token,
+            system_token,
+        ], f"Token {token} is unmasked, special tokens should not be unmasked."
+        assert (
+            token not in [pretrain_token, pretrain_end_token]
+        ), f"Token {token} is a pretraining token, it should not be in the final sentence."
+        assert label in (
+            token,
+            -100,
+        ), f"unless masked, label should be the same as the token."
 
-    # Assert that the conversation ends with an assistant token
-    assert (
-        assist_ids[-1] > user_ids[-1]
-    ), "Conversation does not end with an assistant token"
-
-    # Unmask the final assistant response
-    labels[assist_ids[-1] + 1 : sentence_legth] = whole_sentence[
-        assist_ids[-1] + 1 : sentence_legth
-    ]
-
-    return labels
-
-
-def unmask_only_assistant_responses_from_list(
-    sentence_tk: List[int], user_token: int, assist_token: int
-) -> List[int]:
-    sentence_tk = np.array(sentence_tk)
-    assert sentence_tk.ndim == 1
-
-    if user_token not in sentence_tk or assist_token not in sentence_tk:
-        return sentence_tk.tolist()
-
-    labels = np.full_like(sentence_tk, -100)
-
-    user_ids = (sentence_tk == user_token).nonzero()[0]
-    assist_ids = (sentence_tk == assist_token).nonzero()[0]
-
-    # Find the first user_id that is greater than each assist_id
-    valid_assist_mask = assist_ids[:, None] < user_ids
-    first_user_ids_after_assist_ids = user_ids[valid_assist_mask.argmax(axis=1)]
-
-    # Filter out assist_ids that do not have a corresponding user_id
-    valid_assist_ids = assist_ids[first_user_ids_after_assist_ids != 0]
-    valid_user_ids = first_user_ids_after_assist_ids[
-        first_user_ids_after_assist_ids != 0
-    ]
-
-    # Assign labels for each valid assist_id-user_id pair (without including the assist_id nor the user_id)
-    for assist_id, user_id in zip(valid_assist_ids, valid_user_ids):
-        labels[assist_id + 1 : user_id] = sentence_tk[assist_id + 1 : user_id]
-
-    # Assert that the conversation ends with an assistant token
-    assert (
-        assist_ids[-1] > user_ids[-1]
-    ), "Conversation does not end with an assistant token"
-
-    # Unmask the final assistant response
-    labels[assist_ids[-1] + 1 :] = sentence_tk[assist_ids[-1] + 1 :]
-
-    return labels.tolist()
+    return {"labels": final_labels, "input_ids": final_sentence_tk}
 
 
-def remove_pretrain_system_messages(example: dict):
-    messages = example["messages"]
-    has_pretraining = any(m["role"] == "pretraining" for m in messages)
-    if has_pretraining:
-        messages = [m for m in messages if m["role"] != "system"]
-        assert len(messages) == 1
-    return {"messages": messages}
+def preview_samples(
+    tokenizer, pad_tk, pad_str, labels, input_ids, pretrain_indices, instruct_indices
+):
+    print(
+        "\033[33mThe following are some examples of the processed data, with masked tokens (not to be learned) represented with <mask>. The unmasked tokens are the ones the model will learn to predict. Please review these samples to ensure the model is learning to predict expected tokens.\n\033[0m"
+    )
+    if pretrain_indices:
+        sample_indices = random.sample(pretrain_indices, min(len(pretrain_indices), 2))
+        for idx in sample_indices:
+            label = [pad_tk if tk == -100 else tk for tk in labels[idx]]
+            text = tokenizer.decode(label).replace(pad_str, "<mask>")
+            orig_text = tokenizer.decode(input_ids[idx])
+            print(f"\033[33mPretraining ex sample {idx+1}: {text}\033[0m")
+            print(f"\033[35mOriginal Input: {orig_text}\n\033[0m")
+    if instruct_indices:
+        sample_indices = random.sample(instruct_indices, min(len(instruct_indices), 2))
+        for idx in sample_indices:
+            label = [pad_tk if tk == -100 else tk for tk in labels[idx]]
+            text = tokenizer.decode(label).replace(pad_str, "<mask>")
+            orig_text = tokenizer.decode(input_ids[idx])
+            print(f"\033[33mInstruction ex sample {idx+1}: {text}\033[0m")
+            print(f"\033[35mOriginal Input: {orig_text}\n\033[0m")
+
+
+def form_data_pools(data, pretrain_tk):
+    """
+    Sorts data indices into pretraining and instruction pools
+    """
+    pretrain_indices = []
+    instruct_indices = []
+    for i in tqdm(range(len(data)), desc="Data type sorting"):
+        if pretrain_tk in data[i]:
+            pretrain_indices.append(i)
+        else:
+            instruct_indices.append(i)
+    return pretrain_indices, instruct_indices
 
 
 def main(args: DataProcessArgs):
@@ -208,11 +199,14 @@ def main(args: DataProcessArgs):
         f"eos: {eos_tk}, pad: {pad_tk}, system: {system_tk}, user: {user_tk}, assistant: {assistant_tk}"
     )
 
-    data = load_dataset("json", data_files=args.data_path, split="train")
-    print("\033[92mremoving pretraining samples system msg\033[0m")
-    data = data.map(remove_pretrain_system_messages, num_proc=16)
+    # Adding after tokenizer setup as these are temp tokens, not to be saved
+    tokenizer.add_special_tokens(
+        {"additional_special_tokens": ["<|pretrain|>", "<|/pretrain|>"]}
+    )
 
-    logging.info(f"tokenizing the dataset with {args.model_path} tokenizer...")
+    data = load_dataset("json", data_files=args.data_path, split="train")
+
+    print(f"\033[92mtokenizing the dataset with {args.model_path} tokenizer...\033[0m")
     data_with_input_ids = data.map(
         lambda x: {
             "input_ids": tokenizer.apply_chat_template(x["messages"], tokenize=True)
@@ -245,7 +239,7 @@ def main(args: DataProcessArgs):
         f"\033[36mat 20 min sequence length, the number of samples to be dropped is {num_dropped_samples}\033[0m"
     )
 
-    logging.info("checking the validity of the samples...")
+    print("\033[92mchecking the validity of the samples...\033[0m")
     data_with_input_ids = data_with_input_ids.filter(
         lambda x: check_valid_sample(
             tokenizer,
@@ -262,15 +256,36 @@ def main(args: DataProcessArgs):
         f"\033[33mnumber of dropped samples: {len(data) - len(data_with_input_ids)} -- out of {len(data)}\033[0m"
     )
 
-    logging.info("unmasking the assistant responses...")
+    print("\033[92mCategorizing training data type...\033[0m")
+    pretrain_indices, instruct_indices = form_data_pools(
+        data_with_input_ids["input_ids"],
+        pretrain_tk=get_sp_token(tokenizer, "<|pretrain|>"),
+    )
+
+    _prefill_unmask_message_content = partial(
+        unmask_message_content,
+        user_token=user_tk,
+        assist_token=assistant_tk,
+        system_token=system_tk,
+        pretrain_token=get_sp_token(tokenizer, "<|pretrain|>"),
+        pretrain_end_token=get_sp_token(tokenizer, "<|/pretrain|>"),
+    )
+    print("\033[92munmasking the appropriate message content...\033[0m")
     data_with_labels = data_with_input_ids.map(
-        lambda x: {
-            "labels": unmask_only_assistant_responses_from_list(
-                x["input_ids"], user_tk, assistant_tk
-            )
-        },
+        _prefill_unmask_message_content,
         num_proc=16,
     )
+
+    preview_samples(
+        tokenizer,
+        pad_tk,
+        SPECIAL_TOKENS.pad,
+        data_with_labels["labels"],
+        data_with_labels["input_ids"],
+        pretrain_indices,
+        instruct_indices,
+    )
+
     # extract only labels and messages formatted into a new dataset
     data_with_labels = data_with_labels.select_columns(["labels", "input_ids"])
     # use path to get the stem of the file
