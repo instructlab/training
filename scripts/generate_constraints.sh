050818cff8ebdd24ebce5995d322ba2f9743b9c4
Ihar Hrachyshka <ihar.hrachyshka@gmail.com>
2025-05-21 09:05:48 -0400

    ci: Constrain all dependencies
    Eventually we should adopt (some of this code) from ci-actions that
    should host reusable parts of the constraints infra. The reusable
    doesn't exist at the moment and is being cooked, so in the meantime,
    it's ok to constrain it per repo.
    
    (It also allows us to validate some CUDA specific quirks around
    flash-attn before we commit to the shape of the reusable workflow
    action.)
    
    Signed-off-by: Ihar Hrachyshka <ihar.hrachyshka@gmail.com>

---
+++ scripts/generate_constraints.sh
@@ -0,0 +1,28 @@
+#!/bin/sh
+set -e
+
+# Require Linux; at least until we generate constraints files for each platform
+if [ "$(uname)" != "Linux" ]; then
+    echo "This script is only supported on Linux."
+    exit 1
+fi
+
+CONSTRAINTS_FILE=constraints-dev.txt
+
+pip-compile \
+    --no-header \
+    --annotate \
+    --annotation-style line \
+    --allow-unsafe \
+    --strip-extras \
+    --output-file=$CONSTRAINTS_FILE \
+    --constraint constraints-dev.txt.in \
+    requirements{,-dev,-cuda}.txt
+
+# clean up empty lines and comments
+sed '/^#.*/d' -i constraints-dev.txt
+sed '/^$/d' -i constraints-dev.txt
+
+# pip-compile lists -r requirements.txt twice for some reason: once with
+# relative path and once with absolute. Clean it up.
+sed -E 's/-r \/[^ ]+\/[^,]+, *//' -i $CONSTRAINTS_FILE
